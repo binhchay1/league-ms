83c0664203d99134105f7360ca86ddf32ea30104
tranthuy240814 <tranthuy240814@gmail.com>
2023-10-31 16:31:48 +0700

    create result

--- resources/views/admin/team/index.blade.php
+++ resources/views/admin/team/index.blade.php
@@ -7,7 +7,7 @@
                 @foreach($listTeam as $team)
                     <div class="col-lg-3 mt-2">
                         <div class="" style="background-color: #eff2f4; padding: 5px; margin-bottom: 15px;" >
-                                <h5 class="mt-4" style="text-align: center">{{$team->name}}</h5>
+                                <h5 class="mt-4" style=" text-align: center">{{$team->name}}</h5>
                                <img class="image" src="{{$team->image}}" alt="avatar" style="display: block;margin-left: auto;margin-right: auto;width: 50%; height: 165px; border-radius: 80px">
                                 <a href="{{route('team.show', $team['id'])}}" style="margin-bottom: 10px;width: 70%;margin-left: 40px;" class="btn btn-primary col-sm-12 mt-4 ">Profile</a>
                         </div>
