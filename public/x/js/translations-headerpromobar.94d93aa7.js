6237dec76f4f5d01f0076722f6be7ad0ee1fb90a
tranthuy240814 <tranthuy240814@gmail.com>
2023-11-06 14:28:40 +0700

    fix homepahr

--- public/x/js/translations-headerpromobar.94d93aa7.js
+++
@@ -1,6 +0,0 @@
-window.cjs = window.cjs || {};
-cjs._translates = Object.assign(cjs._translates || {}, {
-    "TRANS_CLOSE": "Đóng",
-    "TRANS_LIVE_STREAMING": "Live Streaming",
-    "TRANS_MULTI_LANGUAGE_DEFAULT": "An English version of Flashscore is available in your region."
-});
--- resources/views/page/footer.blade.php
+++ resources/views/page/footer.blade.php
@@ -1,72 +1,71 @@
-<footer class="footerContainer">
-    <div class="footerContainer__content">
-        <div class="seoFooter">
-            <div class="seoFooter__mainGroup">
-                <div class="seoFooter__categories">
-                    <div class="seoFooter__category">
-                        <div class="seoFooter__categoryTitle"><a href="https://www.flashscore.vn/">BÓNG ĐÁ</a></div>
-                        <div class="seoFooter__categoryLinks">
-                            <div>
-                                <a href="https://www.flashscore.vn/bong-da/asia/afc-champions-league/">AFC Champions
-                                    League 2023</a>
-                            </div>
-                            <div>
-                            </div>
-                        </div>
-                        <div class="seoFooter__category">
-                            <div class="seoFooter__categoryTitle"><a href="https://www.flashscore.vn/tennis/">TENNIS</a>
-                            </div>
-                            <div class="seoFooter__categoryLinks">
-                                <a href="https://www.flashscore.vn/tennis/xep-hang/wta/">Bảng xếp hạng WTA Đơn</a>
-                            </div>
-                        </div>
+<div id="AppFooter">
+    <footer class="footer ">
+        <div class="container">
+            <div class="row ">
+                <div id="inviteGroupFb" class="col-sm-4 col-sm-push-8">
+                    <div id="wrap">
+                        <div id="div1" class></div>
+                        <div id="div2" class></div>
+                        <div class="fb-group" data-href="https://www.facebook.com/groups/myleague.vn/"
+                             data-width="270" data-show-social-context="false" data-show-metadata="true"></div>
                     </div>
-                    <div class="seoFooter__category">
-                        <div class="seoFooter__categoryTitle"><a href="https://www.flashscore.vn/live-scores/">THỂ THAO
-                                THÊM</a></div>
-                        <div class="seoFooter__categoryLinks">
-                            <div>
-                                <a href="https://www.flashscore.vn/bong-bau-duc-my/usa/nfl/#/dd6l0LmE/table/overall">NFL
-                                    - trực tiếp</a>
+                    <div class="applications">
+                        <div class="wrap flex">
+                            <div class="flex-item url"><a href="https://apps.apple.com/vn/app/myleague/id1601852742"
+                                                          target="_blank"> <img
+                                        src="homepage/content/images/apple-app-store-black.png"> </a> <a
+                                    href="https://play.google.com/store/apps/details?id=vn.myleague"
+                                    target="_blank"> <img
+                                        src="homepage/content/images/google-play-store-black.png"> </a>
                             </div>
-                        </div>
-                    </div>
-                    <div class="seoFooter__category">
-                        <div class="seoFooter__categoryTitle"><a href="https://www.flashscore.vn/live-scores/">ĐANG
-                                THỊNH HÀNH</a></div>
-                        <div class="seoFooter__categoryLinks">
-                            <div>
-                                <a href="https://www.flashscore.vn/cau-thu/nguyen-thuy-linh/zi9Vvtp8/">Thùy Linh
-                                    Nguyễn</a>
-                            </div>
-                            <div>
-                                <a href="https://www.flashscore.vn/bong-ro/usa/nba/#/4CS5kbsl/table/overall">NBA
-                                    2023</a>
-                            </div>
-                            <div>
-                                <a href="https://www.flashscore.vn/bong-chay/usa/mlb/">MLB 2023</a>
-                            </div>
-                            <div>
-                                <a href="https://www.flashscore.vn/cau-long/xep-hang/bwf-singles-men/">BWF Đơn Nam</a>
-                            </div>
-                            <div>
-                                <a href="https://www.flashscore.vn/cau-long/xep-hang/bwf-singles-women/">BWF Đơn Nữ</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="seoFooter__category">
-                        <div class="seoFooter__categoryTitle"><a
-                                href="https://www.flashscore.vn/bong-da/england/premier-league/#/I3O5jpB2/table/overall">NGOẠI
-                                HẠNG ANH 2023</a></div>
-                        <div class="seoFooter__categoryLinks">
-                            <div>
-                                <a href="https://www.flashscore.vn/h2h/bong-da/tottenham-UDg08Ohm/chelsea-4fGZN2oK/#/overall">Tottenham
-                                    - Chelsea</a>
+                            <div class="flex-item qr"><img width="110"
+                                                           src="homepage/content/images/app-qr.png">
                             </div>
                         </div>
                     </div>
                 </div>
+                <div id="infoSite" class="col-sm-4 col-sm-pull-4">
+                    <div class="help-block small">
+                        <p class>Nền tảng quản lý giải đấu, đội thi đấu</p>
+                        <ul class="footer-menu">
+                            <li><a href="" class> Điều khoản sử dụng </a></li>
+                            <li><a href="" class> Chính sách bảo mật </a></li>
+                            <br/>
+                            <li><a href="" class> Chính sách thanh toán </a></li>
+                            <li><a href="" class> Liên Hệ </a></li>
+                            <br/>
+                            <li class="policyProfile"><a href="https://myleague.vn/policy-profile" class> Chính sách
+                                    bảo mật thông tin cá nhân </a></li>
+                            <br/>
+                            <li><a href="" class> Nhận xét về MyLeague </a></li>
+                            <li><a href="" class> Bảng giá </a></li>
+                            <br>
+                            <li class="wrap-social-myleague">
+                                <ul class="social-myleague">
+                                    <li>
+                                        <a href="homepage/cdn-cgi/l/email-protection#0f6c60617b6e6c7b4f6276636a6e687a6a217961"><i
+                                                class="fa fa-envelope"></i></a></li>
+                                    <li><a target="_blank" href="https://www.facebook.com/myleague.vn"><i
+                                                class="fa fa-facebook-official"></i></a></li>
+                                    <li><a target="_blank" href="https://instagram.com/myleague.vn"><i
+                                                class="fa fa-instagram"></i></a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div id="wrap-copyright" class="col-sm-4 col-sm-pull-4 text-center">
+                    <a href="/" class="hvr-buzz-out logo ">CÔNG TY CỔ PHẦN MYLEAGUE </a>
+                    <p class="address "><b style="font-size:16px"><i class="fa fa-credit-card"></i></b>: Tầng 8 Tòa
+                        nhà 185 phố Giảng Võ, Phường Cát Linh, Quận Đống Đa, Thành phố Hà Nội, Việt Nam </p>
+                    <p class="phone "><b style="font-size:16px"><i class="fa fa-phone"></i></b>: 024 7307 2628 </p>
+                    <p class="email "><b style="font-size:16px"><i class="fa fa-envelope"></i></b>: <a
+                            href="homepage/cdn-cgi/l/email-protection" class="__cf_email__"
+                            data-cfemail="6c0f0302180d0f182c011500090d0b1909421a02">[email&#160;protected]</a></p>
+                    <p class="small ">
+                        &copy; 2023 - Bản quyền thuộc về myleague.vn <br> <span>Đã đăng ký bản quyền</span></p>
+                </div>
             </div>
         </div>
-    </div>
-</footer>
+    </footer>
+</div>
