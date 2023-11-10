64d5ecf4445397e31b22e5d5973d9ead17ab056c
binhchay1 <binhchay1@gmail.com>
2023-11-10 15:44:06 +0700

    change homepage

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -2,67 +2,238 @@
 <html lang="vi" itemscope itemtype="http://schema.org/Article">
 
 <head>
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-    <meta name="description" content="{{ __('Phần mềm tổ chức, ứng dụng quản lý giải đấu thể thao giúp tiết kiệm thời gian, chi phí và làm cho giải đấu của bạn chuyên nghiệp, thành công hơn.') }}" />
-    <meta name="robots" content="all" />
-    <meta name="keywords" content="{{ __('quản lý giải đấu, tổ chức giải đấu, hệ thống quản lý giải đấu, app quản lý giải đấu, phần mềm quản lý giải đấu, quản lý giải đấu phủi, quản lý giải đấu thể thao, phần mềm tổ chức giải đấu, bóng đá phủi, bóng đá sinh viên, bóng đá học sinh, quản lý giải đấu bóng đá, bóng đá phong trào, bóng đá phủi, bóng đá sân 7, bóng đá sân bảy, bóng đá sân nhân tạo, người quản lý bóng đá, tạo giải đấu game, lịch thi đấu, xếp lịch thi đấu, tổ chức giải đấu, kết quả trận đấu, điểm số, bảng xếp hạng, giải đấu loại trực tiếp, giải đấu vòng tròn, sự kiện giải đấu thể thao, chia bảng đấu online.') }}">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="theme-color" content="#ffffff">
 
-    <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0" />
+    <!-- Favicons -->
+    <link rel="apple-touch-icon-precomposed" href="/assets/images/favicons/favicon-152.png" />
+    <link rel="apple-touch-icon" sizes="57x57" href="/assets/images/favicons/favicon-57.png" />
+    <link rel="apple-touch-icon" sizes="76x76" href="/assets/images/favicons/favicon-76.png" />
+    <link rel="apple-touch-icon" sizes="96x96" href="/assets/images/favicons/favicon-96.png" />
+    <link rel="apple-touch-icon" sizes="120x120" href="/assets/images/favicons/favicon-120.png" />
+    <link rel="apple-touch-icon" sizes="152x152" href="/assets/images/favicons/favicon-152.png" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/images/favicons/favicon-180.png" />
 
-    <title>{{ env('APP_NAME', 'Pro League') }}</title>
-    <link rel="shortcut icon" href="{{ asset('/homepage/content/images/football.ico') }}" type="image/x-icon" />
-    <link href="{{ asset('/homepage/asset/aos/aos.css') }}" rel="stylesheet" />
-    <link href="{{ asset('/homepage/css/app.css') }}" rel="stylesheet" />
+    <link rel="shortcut icon" type="image/png" sizes="16x16" href="/assets/images/favicons/favicon-16.png" />
+    <link rel="shortcut icon" type="image/png" sizes="32x32" href="/assets/images/favicons/favicon-32.png" />
+    <link rel="shortcut icon" type="image/png" sizes="48x48" href="/assets/images/favicons/favicon-48.png" />
+    <link rel="shortcut icon" type="image/png" sizes="64x64" href="/assets/images/favicons/favicon-64.png" />
+    <link rel="shortcut icon" type="image/png" sizes="96x96" href="/assets/images/favicons/favicon-96.png" />
+    <link rel="shortcut icon" type="image/png" sizes="196x196" href="/assets/images/favicons/favicon-196.png" />
 
-    <style>
-        #wrapHomePage #fullpage #home--2:before {
-            background-image: none;
-            background-color: transparent;
+    <meta name="msapplication-TileColor" content="#E45357">
+    <meta name="msapplication-TileImage" content="/assets/images/favicons/favicon-144.png" />
+
+    <meta name="application-name" content="LeagueRepublic">
+    <meta name="msapplication-tooltip" content="LeagueRepublic">
+    <meta name="msapplication-config" content="/assets/images/favicons/browserconfig.xml">
+    <!--  Favicons END -->
+
+    <title>Easily run your sports league: sports league management software</title>
+    <meta name="description" content="Easily manage your league: fixture generator, results, statistics, players and get a professional website." />
+    <meta name="keywords" content="league management software,sports management software,sports organiser,youth sports website builder,best sports website design, league scheduler,fixture generator" />
+    <meta name="robots" content="Index,Follow" />
+    <link rel="canonical" href="https://www.leaguerepublic.com/index.html" />
+    <link rel="stylesheet" type="text/css" href="/assets/css/styles.min.1.8.8.css">
+    <script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/selectivizr/1.0.2/selectivizr-min.js"></script>
+
+
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-J144WP5M2Q"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
         }
+        gtag('js', new Date());
+        gtag('config', 'G-J144WP5M2Q');
+    </script>
 
-        .container-fluid {
-            background-color: #001e28 !important;
-        }
+    <!-- JS END -->
+    <link rel="alternate" hreflang="en-US" href="https://us.leaguerepublic.com/index.html" />
+    <link rel="alternate" hreflang="ms" href="https://ms.leaguerepublic.com/index.html" />
+    <link rel="alternate" hreflang="de" href="https://de.leaguerepublic.com/index.html" />
+    <link rel="alternate" hreflang="es" href="https://es.leaguerepublic.com/index.html" />
+    <link rel="alternate" hreflang="fr" href="https://fr.leaguerepublic.com/index.html" />
+    <link rel="alternate" hreflang="it" href="https://it.leaguerepublic.com/index.html" />
+    <link rel="alternate" hreflang="nl" href="https://nl.leaguerepublic.com/index.html" />
+    <link rel="alternate" hreflang="pt" href="https://pt.leaguerepublic.com/index.html" />
+    <link rel="alternate" hreflang="ro" href="https://ro.leaguerepublic.com/index.html" />
+    <link rel="alternate" hreflang="vi" href="https://vi.leaguerepublic.com/index.html" />
+    <link rel="alternate" hreflang="tr" href="https://tr.leaguerepublic.com/index.html" />
+    <link rel="alternate" hreflang="th" href="https://th.leaguerepublic.com/index.html" />
+    <link rel="alternate" hreflang="ko" href="https://ko.leaguerepublic.com/index.html" />
+    <link rel="alternate" hreflang="x-default" href="https://www.leaguerepublic.com/index.html" />
 
-        .btn-outline {
-            background-color: #0f2d37;
-        }
 
-        a {
-            color: #596377;
-        }
-    </style>
 </head>
 
-<body data-user-id class>
+<body>
+    <header>
+        <div class="top-nav">
+            <ul class="container">
+                <li><a href="/top-sites.html">Top Sites</a></li>
+                <li><a href="https://help.leaguerepublic.com">Help Centre</a></li>
+                <li class="menu">
+                    <span>en</span>
+                    <ul>
+                        <li>
+                            <a class="" href="https://us.leaguerepublic.com/">
+                                English (US)
+                            </a>
+                        </li>
 
-    <div class="bodyContent">
-        <div id="AppBar">
-            @include('page.navbar')
+                        <li>
+                            <a class="" href="https://vi.leaguerepublic.com/">
+                                Tiếng Việt
+                            </a>
+                        </li>
+
+                    </ul>
+                </li>
+            </ul>
         </div>
+        <nav class="container">
+            <a href="/index.html"><img src="/assets/images/lr-logo/logo.svg" alt="LeagueRepublic" width="225" height="52"></a>
+            <button id="toggle-menu" onclick="toggleMobileMenu()"></button>
+            <ul id="menu">
+                <li class="menu">Sports
+                    <ul>
+                        <li>
+                            <a href="/badminton.html">
+                                Badminton
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="/baseball.html">
+                                Baseball
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="/basketball.html">
+                                Basketball
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="/bowling-sports.html">
+                                Bowling Sports
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="/darts.html">
+                                Darts
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="/gaming.html">
+                                Gaming
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="/netball.html">
+                                Netball
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="/pool.html">
+                                Pool
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="/rugby.html">
+                                Rugby
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="/snooker.html">
+                                Snooker
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="/soccer.html">
+                                Soccer
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="/softball.html">
+                                Softball
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="/tennis.html">
+                                Tennis
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="/volleyball.html">
+                                Volleyball
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="/other-sports.html">
+                                Other Sports
+                            </a>
+                        </li>
+
+                    </ul>
+                </li>
+                <li class="menu">Features
+                    <ul>
+                        <li><a href="/features/scheduling.html">League Scheduler</a></li>
+                        <li><a href="/features/results-and-statistics.html">Results & Statistics</a></li>
+                        <li><a href="/features/team-and-player-registration.html">Team & Player Registration</a></li>
+                        <li><a href="/features/site-builder.html">Site Builder</a></li>
+                        <li><a href="/features/league-website-integration.html">Website Integration</a></li>
+                    </ul>
+                </li>
+                <li><a href="/pricing.html">Pricing</a></li>
+                <li id="search">
+                    <form id="searchMenuForm" name="searchMenuForm" action="/search.html" method="post">
+                        <div onclick="openSearch()">
+                            <input type="search" name="searchValue" placeholder="Search leagues...">
+                            <button type="button">
+                                <img src="/assets/images/icons/icon-search.svg" alt="Search" title="Search" width="15" height="15">
+                            </button>
+                        </div>
+                    </form>
+
+
+
+                </li>
+                <li><a href="https://a.leaguerepublic.com/myaccount/login/index.html?lver=1" class="button white">Log In</a></li>
+                <li><a href="https://a.leaguerepublic.com/myaccount/createAccount/1.html?lver=1" class="button">Sign Up</a></li>
+            </ul>
+            <span class="message">
+                Find my league
+            </span>
+        </nav>
+    </header>
+    <div>
         @yield('content')
-        <div id="AppFooter">
-            @include('page.footer')
-        </div>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    <script src="{{ asset('/js/jquery.min.js') }}" type="text/javascript"></script>
-    <script src="{{ asset('/homepage/content/js/vendors/jquery/jquery.ui.timepicker.js') }}" type="text/javascript"></script>
-    <script src="{{ asset('/js/datetimepicker.min.js') }}" type="text/javascript"></script>
-    <script src="{{ asset('/homepage/content/js/vendors/bootstrap/bootstrap.js') }}" type="text/javascript"></script>
-    <script src="{{ asset('/js/dataTables.min.js') }}" type="text/javascript"></script>
-    <script src="{{ asset('/homepage/content/js/vendors/toastr/toastr.min.js') }}" type="text/javascript"></script>
-    <script src="{{ asset('/js/sweetalert2.min.js') }}" type="text/javascript"></script>
-    <script src="{{ asset('/js/lazyIntroCroppiePaceTippy.min.js') }}" type="text/javascript"></script>
-    <script src="{{ asset('/js/photoswipe.min.js') }}" type="text/javascript"></script>
-    <script src="{{ asset('/homepage/content/system/theme/assets/global/plugins/moment.min.js') }}" type="text/javascript"></script>
-    <script src="{{ asset('/homepage/content/system/theme/assets/global/plugins/bootstrap-daterangepicker/daterangepicker.min.js') }}" type="text/javascript"></script>
-    <script src="{{ asset('/homepage/content/js/vendors/exif/exif.min.js') }}" type="text/javascript"></script>
-    <script src="{{ asset('/js/myleague.js') }}" type="text/javascript"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.0/jquery.min.js"></script>
+    <script src="/assets/js/owl.carousel.min.js"></script>
+    <script src="/assets/js/jquery.magnific-popup.min.js"></script>
+    <script src="/assets/js/common.min.1.3.js"></script>
     <script src="{{ asset('/homepage/content/js/vendors/countUp/countUp.js') }}"></script>
-    <script src="{{ asset('/homepage/asset/aos/aos.js') }}"></script>
     @yield('js')
 
 
