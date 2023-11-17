1c2950a742c5e83aeeddcdb559e5fd32a0689c17
tranthuy240814 <tranthuy240814@gmail.com>
2023-11-17 08:29:43 +0700

    detail match

--- resources/views/page/tournament/show.blade.php
+++ resources/views/page/tournament/show.blade.php
@@ -9,7 +9,7 @@
         .tooltip {
             position: relative;
             cursor: default;
-            opacity:1;
+            opacity: 1;
             display: flex;
         }
 
@@ -28,7 +28,6 @@
             border-width: 1px;
             margin-left: 40%;
             margin-bottom: 25%;
-
         }
 
         .tooltip:hover .tooltiptext {
@@ -41,116 +40,170 @@
     <div class="row">
         <div class="col-lg-2">
             <img width="150" class="" src="{{ $tourInfo->image }}">
-
         </div>
-        <div class="col-lg-6">
-            <h2 class="">Power your league with
-                LeagueRepublic</h2>
-            <p class=" ">LeagueRepublic is free to use. We
-                also offer with
-                additional features.</p>
+        <div class="col-lg-6 "style="text-align: left">
+            <h2 class="">{{ $tourInfo->name }}</h2>
+            <p class=" ">{{ $tourInfo->type }}</p>
         </div>
     </div>
 
     <div class="row">
-        <div class="col-lg-2 mt-4">
+        <div class="col-lg-2 " style="margin-top: 35px;">
             <h5>Các giải đấu khác</h5>
-            <hr>
-            @foreach($listTournament as $dataTour)
-                <div style="font-size: 14px; font-weight: 500; padding: 7px;"> <i class='fas fa-baseball-ball'></i>
-                   <a href="{{route('tour.info', $dataTour['name'])}}" style="color: black">
-                       {{ $dataTour->name }}
-                   </a>
-                </div>
+            <div class="mt-4" style="text-align: left">
+                @foreach($listTournament as $dataTour)
+                    <div style="font-size: 14px; font-weight: 500; padding: 7px;"><i class='fas fa-baseball-ball'></i>
+                        <a href="{{route('tour.info', $dataTour['name'])}}" style="color: black">
+                            {{ $dataTour->name }}
+                        </a>
+                    </div>
                 @endforeach
+            </div>
         </div>
         <div class="col-lg-10">
-            <div class="container mt-4" style="background: white; border-radius: 10px">
-    <h2>Chi tiết giải đấu</h2>
-    <div>
-        @foreach($groupSchedule as $match => $schedules)
-        <div style="background: #eee; border-radius: 10px">
-            Vòng đấu: {{ $match }}
-        </div>
-        @foreach($schedules as $schedule)
-        <div class="row mt-4">
-            <div class="col-lg-3">Thời gian: {{ $schedule->time }} {{ $schedule->date }}</div>
-            <div class="col-lg-2">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <img class="image" src="{{ $schedule->team1->image }}" alt="avatar" style=" width: 15px; border-radius: 10px; margin-right: 15px;">
-                        {{ $schedule->team1->name }}
-                    </div>
-                    <div class="col-lg-12 mt-4">
-                        <img class="image" src="{{ $schedule->team2->image }}" alt="avatar" style=" width: 15px; border-radius: 10px; margin-right: 15px;">
-                        {{ $schedule->team2->name }}
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-2">{{ $schedule->stadium }}</div>
-        </div>
-        @endforeach
-        <hr>
-        @endforeach
-
-    </div>
-</div>
-        </div>
-        <div class="modal fade text-left" id="ModalCreate" style="z-index: 99999" tabindex="-1" role="dialog" aria-hidden="true">
-            <div class="modal-dialog modal-lg" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h4 class="modal-title">{{$data->tournament->name}}</h4>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        </button>
-                    </div>
-                    <div class="row justify-content-start m-1 mb-2 mt-2">
-                        <button type="submit" class="btn btn-success " style="width: 90px">{{__('Vòng')}}: {{$data->match}}</button>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-4">
-                            <div class="form-group text-center">
-                                <div class="" style="display: inline-grid;">
-                                    <input value="" type="file" class="border-0 bg-light pl-0" name="image" id="image" hidden>
-                                    <div class=" choose-avatar">
-                                        <div id="btnimage">
-                                            <img class="show-avatar" style="width:150px; height: 150px; border-radius: 50%" src="{{$data->team1->image}}" alt="avatar">
+            <div class="container" style="background: white; border-radius: 10px">
+                <h2>Chi tiết giải đấu</h2>
+                <div>
+                    @forelse($groupSchedule as $match => $schedules)
+                        <div style="background: #eee; border-radius: 10px">
+                            Vòng đấu: {{ $match }}
+                        </div>
+                        @foreach($schedules as $schedule)
+                            <div class="row mt-4 tooltip btn_delete" data-toggle="modal"
+                                 data-target="#ModalCreate " id="evenClick">
+                                <?php $date = date("m-d-Y", strtotime($schedule->date)) ?>
+                                <div class="col-lg-3">Thời gian: {{ $schedule->time }} : {{ $date }}</div>
+                                <div class="col-lg-3">
+                                    <div class="row">
+                                        <div class="col-lg-12">
+                                            <img class="image" src="{{ $schedule->team1->image }}" alt="avatar"
+                                                 style=" width: 15px; border-radius: 10px; margin-right: 15px;">
+                                            {{ $schedule->team1->name }}
+                                        </div>
+                                        <div class="col-lg-12 mt-4">
+                                            <img class="image" src="{{ $schedule->team2->image }}" alt="avatar"
+                                                 style=" width: 15px; border-radius: 10px; margin-right: 15px;">
+                                            {{ $schedule->team2->name }}
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-1">
+                                    <div class="row" style="font-weight: 800">
+                                        <div class="col-lg-12">
+                                            {{ $schedule->result_team_1  ?? "-"}}
+                                        </div>
+                                        <div class="col-lg-12 mt-4">
+                                            {{ $schedule->result_team_2  ?? "-"}}
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4">
+                                    <div class="row">
+                                        <div class="col-lg-12">
+                                            <div class="row">
+                                            <div class="col-lg-2">
+                                                {{ $schedule->set_1_team_1 ?? "-"}}
+                                            </div>
+                                            <div class="col-lg-2">
+                                                {{ $schedule->set_2_team_1 ?? "-"}}
+                                            </div>
+                                            <div class="col-lg-2">
+                                                {{ $schedule->set_3_team_1 ?? "-"}}
+                                            </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-12 mt-4">
+                                            <div class="row">
+                                                <div class="col-lg-2">
+                                                    {{ $schedule->set_1_team_2 ?? "-"}}
+                                                </div>
+                                                <div class="col-lg-2">
+                                                    {{ $schedule->set_2_team_2 ?? "-"}}
+                                                </div>
+                                                <div class="col-lg-2">
+                                                    {{ $schedule->set_3_team_2 ?? "-"}}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="">
+                                    <span class="tooltiptext">Click để xem chi tiết trận đấu</span>
+                                </div>
+                            </div>
+                            <div class="modal fade text-left" id="ModalCreate" style="z-index: 99999" tabindex="-1"
+                                 role="dialog" aria-hidden="true">
+                                <div class="modal-dialog modal-lg" role="document">
+                                    <div class="modal-content" style="height: 400px;">
+                                        <div class="modal-header">
+                                            <h4 class="modal-title">{{$schedule->tournament->name}}</h4>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            </button>
+                                        </div>
+                                        <div class="row justify-content-start m-1 mb-2 mt-2">
+                                            <button type="submit" class="btn btn-success "
+                                                    style="width: 90px">{{__('Vòng')}}: {{$schedule->match}}</button>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-4">
+                                                <div class="form-group text-center">
+                                                    <div class="" style="display: inline-grid;">
+                                                        <input value="" type="file" class="border-0 bg-light pl-0"
+                                                               name="image" id="image" hidden>
+                                                        <div class=" choose-avatar">
+                                                            <div id="btnimage">
+                                                                <img class="show-avatar"
+                                                                     style="width:150px; height: 150px; border-radius: 50%"
+                                                                     src="{{$schedule->team1->image}}" alt="avatar">
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="text-center">
+                                                    <strong>{{$schedule->team1->players[0]->name}}</strong>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-4 text-center"
+                                                 style="vertical-align: middle;line-height: 120px;">
+                                                <?php $date = date("m-d-Y", strtotime($schedule->date)) ?>
+                                                <strong>{{$schedule->time}} &nbsp {{$date}} </strong>
+                                            </div>
+                                            <div class="col-md-4">
+                                                <div class="form-group text-center">
+                                                    <div class="" style="display: inline-grid;">
+                                                        <input value="" type="file" class="border-0 bg-light pl-0"
+                                                               name="image" id="image" hidden>
+                                                        <div class=" choose-avatar">
+                                                            <div id="btnimage">
+                                                                <img class="show-avatar"
+                                                                     style="width: 150px; height: 150px; border-radius: 50% "
+                                                                     src="{{$schedule->team2->image}}" alt="avatar">
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="text-center">
+                                                    <strong>{{$schedule->team2->players[0]->name}}</strong>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <div class="text-center">
-                                <strong>{{$data->team1->players[0]->name}}</strong>
-                            </div>
-                        </div>
-                        <div class="col-lg-4 text-center" style="vertical-align: middle;line-height: 120px;">
-                            <?php $date = date("m-d-Y", strtotime($data->date)) ?>
-                            <strong>{{$data->time}} &nbsp {{$date}} </strong>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="form-group text-center">
-                                <div class="" style="display: inline-grid;">
-                                    <input value="" type="file" class="border-0 bg-light pl-0" name="image" id="image" hidden>
-                                    <div class=" choose-avatar">
-                                        <div id="btnimage">
-                                            <img class="show-avatar" style="width: 150px; height: 150px; border-radius: 50% " src="{{$data->team2->image}}" alt="avatar">
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="text-center">
-                                <strong>{{$data->team2->players[0]->name}}</strong>
-                            </div>
-                        </div>
-                    </div>
+
+                        @endforeach
+
+                        <hr>
+                    @empty
+                        <h5>Chưa cập nhập lịch thi đấu </h5>
+                    @endforelse
 
                 </div>
             </div>
         </div>
-
     </div>
 
     @endsection
-@section('js')
+    @section('js')
     <script src="{{ asset('js/eventSchedule.js') }}"></script>
 @endsection
