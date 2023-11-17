726a0eef33b586f4960fd45395e910b85915138a
tranthuy240814 <tranthuy240814@gmail.com>
2023-11-17 10:52:57 +0700

    add image

--- resources/views/auth/registerUser.blade.php
+++ resources/views/auth/registerUser.blade.php
@@ -10,7 +10,7 @@
                     <div class="card" style="border-radius: 1rem;">
                         <div class="row g-0">
                             <div class="col-md-6 col-lg-5 d-none d-md-block">
-                                <img src="{{ asset('images/sport.png') }}" alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;height: 100%;" />
+                                <img src="{{ asset('images/team/logo.jpg') }}" alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;height: 100%;" />
                             </div>
                             <div class="col-md-6 col-lg-7 d-flex align-items-center">
                                 <div class="card-body p-4 p-lg-5 text-black">
