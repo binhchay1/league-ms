9922aa36b4b12fb782015f824faacaf5ff3aa1f5
tranthuy240814 <tranthuy240814@gmail.com>
2023-11-22 11:56:11 +0700

    multi lang

--- resources/views/admin/league/edit.blade.php
+++ resources/views/admin/league/edit.blade.php
@@ -3,7 +3,7 @@
     <div class="container-fluid mt-4">
         <div class="card card-default">
             <div class="card-header" style="background-color: grey">
-                <h5>{{__('Chỉnh Sửa Giải Đấu')}}</h5>
+                <h5>{{__('Edit league')}}</h5>
             </div>
             <div class="card-body">
                 <form id="formAccountSettings" method="POST" action="{{ route('league.update',$dataLeague['id']) }}" enctype="multipart/form-data">
@@ -11,7 +11,7 @@
                     <div class="row">
                         <div class="col-md-4 mt-4">
                             <div class="form-group">
-                                <label>{{__('Logo giải đấu')}}</label>
+                                <label>{{__('Logo league')}}</label>
                                 <div class="">
                                     <div class="" style="display: inline-grid;">
                                         <input value="" type="file" class="border-0 bg-light pl-0" name="image" id="image" hidden>
@@ -20,7 +20,7 @@
                                                 <img id="showImage" class="show-avatar" src="{{$dataLeague->image ?? asset('/images/champion.png') }}" alt="avatar" style="width: 200px; margin-left: 40px">
                                             </div>
                                             <div id="button" >
-                                                <i id="btn_chooseImg" class="fas fa-camera">  {{__('Chọn ảnh')}}</i>
+                                                <i id="btn_chooseImg" class="fas fa-camera">  {{__('Choose Image')}}</i>
                                             </div>
                                         </div>
                                         @if ($errors->has('image'))
@@ -34,7 +34,7 @@
                         <div class="col-md-6">
                             <div class="row mt-4">
                                 <div class="col-6">
-                                    <label for="lastName" class="form-label">{{__('Tên giải đấu')}}</label>
+                                    <label for="lastName" class="form-label">{{__('Name')}}</label>
                                     <input class="form-control" value="{{$dataLeague->name}}" type="text" name="name" id="name"  />
                                     @if ($errors->has('name'))
                                         <span class="text-danger">{{ $errors->first('name') }}</span>
@@ -51,7 +51,7 @@
                             <div class="row mt-4">
                                 <div class="col-6">
                                     <div class="form-group">
-                                        <label for="address" class="form-label">{{__('Ngày bắt đầu')}}</label>
+                                        <label for="address" class="form-label">{{__('Start date')}}</label>
                                         <input type="date" value="{{$dataLeague->start_date}}" class="form-control" id="start_date" name="start_date" placeholder="Address" />
                                         @if ($errors->has('start_date'))
                                             <span class="text-danger">{{ $errors->first('start_date') }}</span>
@@ -60,7 +60,7 @@
                                 </div>
                                 <div class="col-6">
                                     <div class="form-group">
-                                        <label for="address" class="form-label">{{__('Ngày kết thúc')}}</label>
+                                        <label for="address" class="form-label">{{__('End date')}}</label>
                                         <input type="date" value="{{$dataLeague->end_date}}" class="form-control"  id="end_date" name="end_date" placeholder="Address" />
                                         @if ($errors->has('end_date'))
                                             <span class="text-danger">{{ $errors->first('end_date') }}</span>
@@ -70,16 +70,16 @@
                             </div>
 
                             <div class="mt-4">
-                                <label for="lastName" class="form-label">{{__('Đội tham gia')}}</label>
-                                <input class="form-control" value="{{$dataLeague->number_of_team}}" type="text" name="number_of_team" id="number_of_team"  />
-                                @if ($errors->has('number_of_team'))
-                                    <span class="text-danger">{{ $errors->first('number_of_team') }}</span>
+                                <label for="lastName" class="form-label">{{__('Number of athletes')}}</label>
+                                <input class="form-control" value="{{$dataLeague->number_of_athletes}}" type="text" name="number_of_athletes" id="number_of_athletes"  />
+                                @if ($errors->has('number_of_athletes'))
+                                    <span class="text-danger">{{ $errors->first('number_of_athletes') }}</span>
                                 @endif
                             </div>
                             <div class="row mt-4">
                                 <div class="col-6">
                                     <div class="form-group">
-                                        <label for="lastName" class="form-label">{{__('Địa điểm')}}</label>
+                                        <label for="lastName" class="form-label">{{__('Location')}}</label>
                                         <input class="form-control" value="{{$dataLeague->location}}" type="text" name="location" id="location"  />
                                         @if ($errors->has('location'))
                                             <span class="text-danger">{{ $errors->first('location') }}</span>
@@ -88,7 +88,7 @@
                                 </div>
                                 <div class="col-6">
                                     <div class="form-group">
-                                        <label for="lastName" class="form-label">{{__('Tiền thưởng')}}</label>
+                                        <label for="lastName" class="form-label">{{__('Prize money')}}</label>
                                         <input class="form-control" value="{{$dataLeague->money}}" type="number" name="money" id="money"  />
                                         @if ($errors->has('money'))
                                             <span class="text-danger">{{ $errors->first('money') }}</span>
@@ -99,7 +99,7 @@
                             <div class="row mt-4">
                                 <div class="col-6">
                                     <div class="form-group">
-                                        <label>{{__('Thể thức thi đấu')}}</label>
+                                        <label>{{__('Format of league')}}</label>
                                         <select id="format_of_league" value="{{$dataLeague->format_of_league}}" name="format_of_league" class="form-control select2 select2-danger" data-dropdown-css-class="select2-danger" style="width: 100%;">
                                             @foreach($format_tours as $format_tour => $value)
                                                 <option id="format_of_league" value="{{$value}}">{{$value}}</option>
@@ -109,7 +109,7 @@
                                 </div>
                                 <div class="col-6">
                                     <div class="form-group">
-                                        <label>{{__('Hình thức thi đấu')}}</label>
+                                        <label>{{__('Type of league')}}</label>
                                         <select  id="type_of_league" value="{{$dataLeague->type_of_league}}" name="type_of_league" class="form-control select2 select2-danger" data-dropdown-css-class="select2-danger" style="width: 100%;">
                                             @foreach($type_tours as $type_tour => $value)
                                                 <option id="type_of_league" value="{{$value}}">{{$value}}</option>
@@ -121,8 +121,8 @@
                         </div>
                     </div>
                     <div class="mt-4">
-                        <button type="submit" class="btn btn-primary me-2">{{__('Lưu')}}</button>
-                        <button type="reset" class="btn btn-outline-secondary">{{__('Hủy')}}</button>
+                        <button type="submit" class="btn btn-primary me-2">{{__('Save')}}</button>
+                        <button type="reset" class="btn btn-outline-secondary">{{__('Cancel')}}</button>
                     </div>
                 </form>
             </div>
--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,8 +1,8 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
-    All rights reserved.
+    <strong>{{__('Copyright &copy; 2014-2019')}} <a href="http://adminlte.io">{{__('AdminLTE.io')}}</a>.</strong>
+    {{__('All rights reserved.')}}
     <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 3.0.2
+        <b>{{__('All rights reserved.')}}</b>
     </div>
 </footer>
 
