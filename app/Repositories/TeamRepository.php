297731c27d940ad30abfe8e3a67c876ee8e04030
tranthuy240814 <tranthuy240814@gmail.com>
2023-11-23 17:20:13 +0700

    fix

--- app/Repositories/TeamRepository.php
+++ app/Repositories/TeamRepository.php
@@ -4,7 +4,7 @@ namespace App\Repositories;
 
 use App\Models\UserLeague;
 
-class UserLeagueRepository extends BaseRepository
+class TeamRepository extends BaseRepository
 {
     public function model()
     {
