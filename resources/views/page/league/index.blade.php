e1de055e8ac47ac2d3236c95cdfaffcd53fe63be
tranthuy240814 <tranthuy240814@gmail.com>
2023-11-29 15:57:57 +0700

    fix layout league homepage

--- resources/views/page/league/index.blade.php
+++ resources/views/page/league/index.blade.php
@@ -11,29 +11,27 @@
 
     <div class="container">
         <h2>{{__('List League')}}</h2>
-        <div  style="background: white; border-radius: 10px; padding: 10px">
+        <div>
             @foreach($listLeague as $listLeague)
-                <div class="te" style=" margin-top: 10px">
-                    <div id="" style="background: #eee;" >
-                        <a href="{{route('tour.info', $listLeague['slug'])}}" style="text-decoration: none;">
-                            <div class="row" style="color: black; height: 200px;">
-                                <div class="col-lg-3">
-                                    <img class="lazy truncated initial loaded white center" src="{{$listLeague->image}}" style="width: 200px;height: 150px;margin: 30px">
-                                </div>
-                                <div class="col-7 mt-4">
-                                    <h3 class="" >{{ $listLeague->name }}</h3>
-
-                                    <h5 class="">{{__('Start Date')}}: {{ $listLeague->start_date }}</h5>
-                                    <h5 class="">{{__('End Date')}}: {{ $listLeague->end_date }}</h5>
-                                    <p class="">{{__('PRIZE MONEY USD ')}}${{ $listLeague->money }}</p>
-                                </div>
-                                <div class="col-lg-2">
-                                    <img class="" src="{{$listLeague->image_nation_flag}}" style="width: 100px;height: 50px; margin-top: 30px">
-                                    <h6 class="mt-4" >{{$listLeague->national}}</h6>
-                                </div>
+                <div style="background: #aeaeae; margin-top: 10px"  onmouseover="this.style.background='gray';" onmouseout="this.style.background='#aeaeae';" >
+                    <a href="{{route('tour.info', $listLeague['slug'])}}" class="nounderline" >
+                        <div class="row" style="color: black; height: 200px;">
+                            <div class="col-lg-3">
+                                <img class="lazy truncated initial loaded white center " src="{{$listLeague->image}}" style="width: 200px;height: 150px;margin: 30px">
                             </div>
-                        </a>
-                    </div>
+                            <div class="col-7 mt-4">
+                                <h3 class="" >{{ $listLeague->name }}</h3>
+
+                                <h5 class="">{{__('Start Date')}}: {{ $listLeague->start_date }}</h5>
+                                <h5 class="">{{__('End Date')}}: {{ $listLeague->end_date }}</h5>
+                                <p class="">{{__('PRIZE MONEY USD ')}}${{ $listLeague->money }}</p>
+                            </div>
+                            <div class="col-lg-2">
+                                <img class="" src="{{$listLeague->image_nation_flag}}" style="width: 100px;height: 50px; margin-top: 30px">
+                                <h6 class="mt-4" >{{$listLeague->national}}</h6>
+                            </div>
+                        </div>
+                    </a>
                 </div>
             @endforeach
         </div>
--- resources/views/page/league/show.blade.php
+++ resources/views/page/league/show.blade.php
@@ -3,95 +3,80 @@
 @section('title')
 {{__('Detail League')}}
 @endsection
-
 @section('content')
-<style>
-    .zoom-img img {
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-        transition: all .3s ease;
-    }
 
-    .zoom-img img:hover {
-        transform: scale(1.2);
-    }
-</style>
-<div class="container">
-    <div style="background: #eee">
-        <div id="loading" style="background-image: url('{{ asset('images/badminton.jpg')}}'); height: 300px; margin-top: -25px; background-repeat: no-repeat;">
-            <div class="" style="border-radius: 10px; padding: 10px">
-                <div class="row">
-                    <div class="col-lg-2 mt-4">
-                        <img width="150" class="" src="{{ $tourInfo->image }}">
+<div id="page" class="hfeed site">
+    <div class="">
+        <div class="container-1280 results">
+
+            <div class="std-title">
+                <div class="std-title-left">
+                    <h2 class="left">{{__('LEAGUE INFORMATION')}}</h2>
+                </div>
+                <div class="std-title-center">
+                </div>
+                <div class="std-title-right">
+                </div>
+            </div>
+            <div class="wrapper-results">
+                <div class="box-title page-header">
+                    <div class="box-title-left">
                     </div>
-                    <div class="col-lg-6 " style="text-align: left">
-                        <h2 class="">{{ $tourInfo->name }}</h2>
-                        <h5 class="">{{__('Start Date')}}: {{ $tourInfo->start_date }}</h5>
-                        <h5 class="">{{__('End Date')}}: {{ $tourInfo->end_date }}</h5>
-                        <p class="">{{__('PRIZE MONEY USD ')}}${{ $tourInfo->money }}</p>
+                    <div class="box-title-right">
+                        <h4 class="right">
+                            OTHER WORLD TOUR FINALS RESULTS </h4>
+
+                        <label class="tournament-select clear">
+                            <select class="ddlTournament" >
+                                <option value="">HSBC BWF World Tour Finals 2022</option>
+                                <option value="">HSBC BWF World Tour Finals 2021(New dates)</option>
+                                <option value="">HSBC BWF World Tour Finals 2020 (New Dates)</option>
+                                <option value="">HSBC BWF World Tour Finals 2019</option>
+                                <option value="">HSBC BWF World Tour Finals 2018</option>
+                            </select>
+                        </label>
+                    </div>
+                </div>
+                <div class="box-results-tournament">
+                    <div>
+                        <div class="logo-left">
+                            <img height="90" src="{{ $tourInfo->image }}" alt="logo">
+                        </div>
+                        <div class="info">
+                            <h2>{{ $tourInfo->name }}</h2>
+                            <h5>{{__('Start Date')}}: {{ $tourInfo->start_date }}</h5>
+                            <h5>{{__('End Date')}}: {{ $tourInfo->end_date }}</h5>
+                            <div class="prize">{{__('PRIZE MONEY USD ')}}${{ $tourInfo->money }}</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="wrapper-content-results" style="padding: 0px; margin-top: 18px;">
+                    <ul id="ajaxTabs" class="content-tabs">
+                        <li>
+                            <a href="">{{__('Result')}} </a>
+                        </li>
+                        <li><a href="">{{__('Schedule')}}</a>
+                        </li>
+                        <li><a href="">{{__('Fighting Branch')}}</a>
+                        </li>
+                        <li><a href="">{{__('Player ')}}</a>
+                        </li>
+                    </ul>
+                    <div class="content-results">
+
+                        <div class="item-results">
+                            @include('page.league.detail.player')
+                            @include('page.league.detail.result')
+                            @include('page.league.rank')
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav" style="font-size: 20px; font-weight: 600;">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="">{{__('Result')}}</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">{{__('Schedule')}}</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">{{__('Fighting Branch')}}</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">{{__('Player')}}</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
     </div>
+</div>
 
-    <hr>
-
-    {{--Content League --}}
-    {{--Result --}}
-    {{--Schedule--}}
-    {{--Fighting Branch --}}
-    {{--Player --}}
-    <div id="page" class="hfeed site">
-        <section id="ranking" class="container-1280 rankings-section pb-200">
-            <div class="tab-content rankings-content_tabpanel">
-                <h3 style="margin-bottom: 12px;">{{__('Player')}}</h3>
-                <div class="entry-player-all-wrap">
-                    @foreach($tourInfo->userLeagues as $listTour)
-                    <div class="entry-player-pair-wrap">
-                        <a href="" style="text-decoration: none;">
-                            <div class="entry-player-wrap">
-                                <div class="entry-player-image">
-                                    <img width="80" class="lazy truncated initial loaded white " src="{{$listTour->user->image ?? '/images/no-image.png'}}" style="width: 100px; height: 125px">
-                                </div>
-                                <div class="entry-player-info-wrap">
-                                    <div class="entry-player-name">
-                                        {{$listTour->user->name}}
-                                    </div>
-                                    <div class="entry-player-flag">
-                                        <img src="https://extranet.bwf.sport/docs/flags/indonesia.png" />
-                                        <span>Indonesia</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                    @endforeach
-                </div>
-            </div>
-        </section>
-    </div>
-
-    @endsection
-    @section('js')
+@endsection
+@section('js')
     <script src="{{ asset('js/eventSchedule.js') }}"></script>
-    @endsection
+@endsection
