c81ea8c5bcc30d976604b7e490b4f428e8214ff9
binhchay1 <binhchay1@gmail.com>
2023-12-13 17:08:06 +0700

    fix middleware

--- app/Http/Middleware/isAdmin.php
+++ app/Http/Middleware/isAdmin.php
@@ -17,7 +17,7 @@ class isAdmin
      */
     public function handle(Request $request, Closure $next)
     {
-        if (Auth::user() ) {
+        if (Auth::user()) {
             return $next($request);
         }
 
