035fe8c21e0a0d07cb99ad69a0b88e8f003efc16
binhchay1 <binhchay1@gmail.com>
2023-12-26 14:36:22 +0700

    change user

--- app/Http/Controllers/Admin/ScheduleController.php
+++ app/Http/Controllers/Admin/ScheduleController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Admin;
 use App\Enums\Ranking;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ResultScheduleRequest;
-use App\Http\Requests\ScheduleRequest;
 use App\Repositories\ScheduleRepository;
 use App\Repositories\LeagueRepository;
 use Illuminate\Http\Request;
@@ -29,46 +28,52 @@ class ScheduleController extends Controller
         $listLeagues = $this->leagueRepository->index();
         $rounds =  Ranking::RANKING_ARRAY_ROUND;
         $listSchedules = $this->scheduleRepository->index();
+
         return view('admin.schedule.index', compact('listSchedules', 'listLeagues', 'rounds'));
     }
 
     public function league()
     {
         $listLeagues = $this->leagueRepository->index();
+
         return view('admin.schedule.list-league', compact('listLeagues'));
     }
 
     public function leagueSchedule($id)
     {
         $league = $this->leagueRepository->show($id);
-
         $rounds =  Ranking::RANKING_ARRAY_ROUND;
+
         return view('admin.schedule.create', compact('league', 'rounds'));
     }
 
     public function store(Request $request)
     {
         $input = $request->except(['_token']);
-        $data = $this->scheduleRepository->store($input);
+        $this->scheduleRepository->store($input);
+
         return redirect('list-schedule')->with('success', 'Create schedule successfully!');
     }
 
     public function show($id)
     {
         $dataSchedule = $this->scheduleRepository->showInfo($id);
+
         return view('admin.schedule.show', compact('dataSchedule'));
     }
 
     public function update(ResultScheduleRequest $request, $id)
     {
         $input = $request->except(['_token']);
-        $data = $this->scheduleRepository->update($input, $id);
+        $this->scheduleRepository->update($input, $id);
+
         return redirect()->to('result');
     }
 
     public function result()
     {
         $dataResult = $this->scheduleRepository->index();
+
         return view('admin.schedule.result', compact('dataResult'));
     }
 }
