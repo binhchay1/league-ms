ea197ca3edd59e87ae2447c8f0f2b39e97da2d21
binhchay1 <binhchay1@gmail.com>
2024-01-12 10:32:09 +0700

    fix mail

--- config/mail.php
+++ config/mail.php
@@ -42,14 +42,7 @@ return [
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
-            'auth_mode' => null,
-            'stream' => [
-                'ssl' => [
-                    'allow_self_signed' => true,
-                    'verify_peer' => false,
-                    'verify_peer_name' => false,
-                ],
-            ]
+            'auth_mode' => null
         ],
 
         'ses' => [
