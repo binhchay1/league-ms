ad3e291acc0fc1ec6838219e1de4b48578f6bb3c
binhchay1 <binhchay1@gmail.com>
2024-04-03 11:42:58 +0700

    change composer

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,8 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Lcobucci\JWT\Configuration;
-use Lcobucci\JWT\Signer\Ecdsa\Sha256;
-use Lcobucci\JWT\Signer\Key\InMemory;
+use Illuminate\Support\Facades\Process;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -23,19 +22,9 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $secret = [
-            '-----BEGIN PRIVATE KEY-----',
-            'MIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQg7ktNQa6pxH4zGHKb',
-            '/zRMAps/ke47YlZuXieO8kBRh1ygCgYIKoZIzj0DAQehRANCAAQHO+5+fdn3Zmg+',
-            'yMfTFssQpMxmma1PkOaPK+6zMzhB0O3Ca4F9LkxQnkabTV1I7vZPq3AwynGRXBag',
-            '5QDWUs+s',
-            '-----END PRIVATE KEY-----',
-        ];
+        $result = Process::run('ruby client_secret.rb');
+        $output = $result->output();
 
-        $implode = implode($secret);
-        $this->app->bind(Configuration::class, fn () => Configuration::forSymmetricSigner(
-            signer: new Sha256(),
-            key: InMemory::plainText($implode),
-        ));
+        $this->app->bind(Configuration::class, $output);
     }
 }
