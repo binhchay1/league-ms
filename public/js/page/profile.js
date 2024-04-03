51e2ddd0fcb6a8203d0267c052d705a9b46af52a
binhchay1 <binhchay1@gmail.com>
2024-04-03 16:42:41 +0700

    change trigger

--- public/js/page/profile.js
+++ public/js/page/profile.js
@@ -26,25 +26,12 @@ function deleteAccount() {
     window.location.href = '/delete-account-apple/';
 }
 
-navigator.getUserMedia =
-    navigator.getUserMedia ||
-    navigator.webkitGetUserMedia ||
-    navigator.mozGetUserMedia;
-
-if (navigator.getUserMedia) {
-    navigator.getUserMedia(
-        { audio: true, video: { width: 1280, height: 720 } },
-        (stream) => {
-            const video = document.querySelector("video");
-            video.srcObject = stream;
-            video.onloadedmetadata = (e) => {
-                video.play();
-            };
-        },
-        (err) => {
-            console.error(`The following error occurred: ${err.name}`);
-        },
-    );
-} else {
-    console.log("getUserMedia not supported");
-}
+navigator.mediaDevices
+    .getUserMedia({ video: true, audio: true })
+    .then((localMediaStream) => {
+        const video = document.querySelector("video");
+        video.srcObject = localMediaStream;
+    })
+    .catch((error) => {
+        console.log("Rejected!", error);
+    });
