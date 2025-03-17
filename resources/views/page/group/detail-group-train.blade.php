b90e61f60e52f8073ac3a28e9db815c6e807d932
tranthuy98 <tranthuy240814@gmail.com>
2025-03-17 14:26:47 +0700

    featur-update-layout-group-league

--- resources/views/page/group/detail-group-train.blade.php
+++ resources/views/page/group/detail-group-train.blade.php
@@ -7,63 +7,129 @@
 @section('css')
 <meta name="csrf-token" content="{{ csrf_token() }}" />
 <link rel="stylesheet" href="{{ asset('/css/page/group.css') }}" />
+    <style>
+
+        .filter-box {
+            border-radius: 8px;
+            display: flex;
+            gap: 10px;
+            flex-wrap: wrap;
+        }
+        .form-select {
+            border-radius: 6px;
+            border: 1px solid #ccc;
+        }
+        .table-box {
+            border-radius: 10px;
+            overflow: hidden;
+        }
+        .table {
+            border-radius: 10px;
+            background: white;
+            margin-bottom: 0;
+        }
+        .table thead {
+            background: #f1f1f1;
+        }
+        .table th, .table td {
+            border-bottom: 1px solid #ddd;
+            padding: 10px;
+        }
+        .avatar {
+            width: 40px;
+            height: 40px;
+            border-radius: 50%;
+            object-fit: cover;
+            margin-right: 10px;
+        }
+    </style>
 @endsection
 
 @section('content')
-<div class="container">
-    <div class="card">
-        <div class="card-header group-name">
-            {{ $groupTrainingDetail->name }}
-        </div>
+<div class="">
+    <?php   $date = date("d/m/Y", strtotime($groupTrainingDetail->date));
+    $start_time= date("H:i", strtotime($groupTrainingDetail->start_time));
+    $end_time = date("H:i", strtotime($groupTrainingDetail->end_time));
+    ?>
+    <div>
+        <div class=" text-black p-3 align-items-center">
+            <div class="container d-flex  img-fluid">
+                <img src="{{ asset($groupTrainingDetail->groups->images) }}" alt="User" width="200" height="200" class=" me-3 rounded-start" >
+                <div>
+                    <h5 class="">{{$groupTrainingDetail->name}}</h5>
+                    <p class="">
+                        <i class="bi bi-bookmark"></i> {{$groupTrainingDetail->description}}
+                    </p>
+                    <p class="">
+                        <i class="bi bi-geo-alt"></i> <em>{{$groupTrainingDetail->location}}</em>
+                    </p>
+                    <p class="">
+                        <i class="bi bi-calendar-event"></i> <em>{{ $date }}</em>
+                    </p>
+                    <p class="">
+                        <i class="bi bi-calendar-check"></i> <em>{{ $start_time}} ~  {{$end_time}}</em>
+                    </p>
+                    <p class="">
+                        <i class="bi bi-card-text"></i> <em>{{$groupTrainingDetail->note}}</em>
+                    </p>
 
-        <?php   $date = date("d/m/Y", strtotime($groupTrainingDetail->date));
-        $start_time= date("H:i", strtotime($groupTrainingDetail->start_time));
-        $end_time = date("H:i", strtotime($groupTrainingDetail->end_time));
-        ?>
+                    <div style="width: 200%;">
 
-        <div class="card-body">
-            <p>■  {{ __('Description') }}: {{ $groupTrainingDetail->description }}</p>
-            <p>■  {{ __('Location') }}: {{ $groupTrainingDetail->location }}</p>
-            <p>■  {{ __('Date') }}: {{ $date }}</p>
-            <p>■  {{ __('Number of member') }}: {{ $groupTrainingDetail->location }}</p>
-            <p>■  {{ __('Activity time') }}: {{ $start_time}} ~  {{$end_time}}</p>
-            <p class="fst-italic fw-light fw-bold">----- {{ __('Note') }}: {{ $groupTrainingDetail->note }}</p>
-        </div>
-        <div class="mt-3" style="padding: 15px">
-            @if(!empty($listMembers))
-            <div class="progress">
-                <div class="progress-bar" role="progressbar" <?php echo 'style="width:' . ($listMembers->count() / $groupTrainingDetail->number_of_members * 100) . '%"' ?> aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-            </div>
-            <div class="d-flex justify-content-between mt-3">
-                <div> <span class="text1">{{ $listMembers->count() }} {{ __('Applied') }} <span class="text2">of {{ $groupTrainingDetail->number_of_members }}</span></span> </div>
-            </div>
-            @else
-            <div class="progress">
-                <div class="progress-bar" role="progressbar" style="width: 0;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-            </div>
-            <div class="d-flex justify-content-between mt-3">
-                <div> <span class="text1">0 {{ __('Applied') }} <span class="text2">of {{ $groupTrainingDetail->number_of_members }}</span></span> </div>
-            </div>
-            @endif
+                        @if(!empty($listMembers))
+                            <div class="progress">
+                                <div class="progress-bar" role="progressbar" <?php echo 'style="width:' . ($listMembers->count() / $groupTrainingDetail->number_of_members * 100) . '%"' ?> aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                            <div class="d-flex justify-content-between mt-3">
+                                <div> <span class="text1">{{ $listMembers->count() }} {{ __('Applied') }} <span class="text2">of {{ $groupTrainingDetail->number_of_members }}</span></span> </div>
+                            </div>
+                        @else
+                            <div class="progress">
+                                <div class="progress-bar" role="progressbar" style="width: 0;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                            <div class="d-flex justify-content-between mt-3">
+                                <div> <span class="text1">0 {{ __('Applied') }} <span class="text2">of {{ $groupTrainingDetail->number_of_members }}</span></span> </div>
+                            </div>
+                        @endif
 
+                    </div>
+                </div>
+            </div>
         </div>
+        <hr>
     </div>
 
-    <div class="card" style="width: 50rem; margin-top: 20px; margin-bottom: 120px">
-        <div class="d-flex">
-            <div class=" align-items-center" >
-                <img class="avatar-group" src="{{ asset('https://png.pngtree.com/png-clipart/20230817/original/pngtree-badminton-icon-logo-and-sport-club-template-vector-vector-picture-image_10923178.png')  }}">
-            </div>
-            <div class="" >
-                <h5 style="line-height: 50px">{{__('Join')}}</h5>
+        <div class="container p-0  mt-4 table-join" >
+            <div class="">
+                <div class="d-flex">
+                    <button class="btn btn-primary">{{'Member Join'}}</button>
+                </div>
+                <!-- Bảng danh sách -->
+                <div class="table-box mt-4">
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <th>{{'MEMBER'}}</th>
+                            <th>{{'EMAIL'}}</th>
+                            <th>{{'PHONE'}}</th>
+                            <th>{{'ADDRESS'}}</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            @foreach($listMembers as $member)
+                                <td>
+                                    <img src="{{ $member->users->profile_photo_path ?? asset('/images/no-image.png')  }}" width="40" height="40"  alt="avatar" class="avatar">
+                                    {{ $member->name }}
+                                </td>
+                                <td><a href="#">{{$member->email}}</a></td>
+                                <td>{{$member->phone}}</td>
+                                <td>{{$member->address}}</td>
+                            @endforeach
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
-
-        <ul class="list-group list-group-flush">
-            @foreach($listMembers as $members)
-            <li class="list-group-item">{{ $members->name }}</li>
-            @endforeach
-        </ul>
-    </div>
 </div>
 @endsection
--- resources/views/page/user/my-league.blade.php
+++ resources/views/page/user/my-league.blade.php
@@ -1,82 +1,3 @@
-{{--@extends('layouts.page')--}}
-
-{{--@section('title')--}}
-{{--    {{ env('APP_NAME', 'Badminton.io') }} - {{ __('My League') }}--}}
-{{--@endsection--}}
-
-{{--@section('css')--}}
-{{--    <link rel="stylesheet" href="{{ asset('/css/page/my-league.css') }}">--}}
-{{--@endsection--}}
-{{--<style>--}}
-
-{{--    .tournament-card {--}}
-{{--        border-radius: 10px;--}}
-{{--        overflow: hidden;--}}
-{{--        box-shadow: 1px 4px 8px rgba(0.1, 0.1, 0.1, 0.1);--}}
-{{--        background-color: white;--}}
-{{--        padding: 15px;--}}
-{{--        margin-bottom: 15px;--}}
-{{--    }--}}
-{{--    .tournament-logo {--}}
-{{--        width: 60px;--}}
-{{--        height: 60px;--}}
-{{--        border-radius: 10px;--}}
-{{--        object-fit: cover;--}}
-{{--    }--}}
-{{--    .status-badge {--}}
-{{--        padding: 5px 10px;--}}
-{{--        border-radius: 5px;--}}
-{{--        font-size: 14px;--}}
-{{--    }--}}
-{{--</style>--}}
-{{--@section('content')--}}
-
-{{--    <div class="container mt-4">--}}
-{{--        <div class="d-flex justify-content-between align-items-center mb-3">--}}
-{{--            <h3>{{ __('My League') }}</h3>--}}
-{{--            <a href="{{route('league.createTour')}}">--}}
-{{--                <button class="btn btn-success">{{ __('Create League') }}</button>--}}
-{{--            </a>--}}
-
-{{--        </div>--}}
-{{--        <div class="row">--}}
-{{--            @if(count($listLeague) > 0)--}}
-{{--                @foreach($listLeague as $row)--}}
-{{--            <div class="col-md-12">--}}
-{{--                <div class="tournament-card d-flex align-items-center">--}}
-{{--                    <img src="{{ asset($row-> images ?? 'https://png.pngtree.com/png-clipart/20230817/original/pngtree-badminton-icon-logo-and-sport-club-template-vector-vector-picture-image_10923178.png')  }}" alt="Logo" class="tournament-logo me-3">--}}
-{{--                    <div class="flex-grow-1">--}}
-{{--                        <a href="{{route('league.info', $row->slug)}}">--}}
-{{--                            <h5>{{ $row->name }}</h5>--}}
-{{--                        </a>--}}
-{{--                        <p class="mb-1"><?php echo number_format($row->money ?? 0) . " VND"?> || {{$row->type_of_league}}  || {{$row->location}}</p>--}}
-
-{{--                        <button  class="btn btn-{{$row->status ? 'info' : 'secondary' }}">--}}
-{{--                            {{$row->status ? "Activated" : "Inactive"}}--}}
-{{--                        </button>--}}
-{{--                    </div>--}}
-
-{{--                </div>--}}
-{{--            </div>--}}
-{{--                @endforeach--}}
-{{--            @else--}}
-{{--                <div class="text-center">--}}
-{{--                    <img class="" width="200" height="200" src="{{ asset('/images/logo-no-background.png') }}">--}}
-
-{{--                    <h4 >{{ __('There are no leagues!') }}</h4>--}}
-{{--                </div>--}}
-{{--            @endif--}}
-{{--        </div>--}}
-{{--    </div>--}}
-
-{{--@endsection--}}
-
-{{--@section('js')--}}
-{{--@endsection--}}
-
-
-
-
 @extends('layouts.page')
 
 @section('title')
@@ -151,10 +72,9 @@
                     <!-- Sidebar -->
                     <div class="col-md-3 p-3 bg-light">
                         <div class="list-group">
-                            <a href="#" class="list-group-item list-group-item-action active">Giải đấu đã tạo</a>
-                            <a href="#" class="list-group-item list-group-item-action">Được phân công</a>
-                            <a href="#" class="list-group-item list-group-item-action">Đang quan tâm</a>
-                            <a href="#" class="list-group-item list-group-item-action">Đang tham gia</a>
+                            <a href="#" data-id="league-created" class="list-group-item list-group-item-action active">{{'League created'}}</a>
+                            <a href="#" data-id="league-assigned" class="list-group-item list-group-item-action">{{'League assigned'}}</a>
+                            <a href="#"  data-id="league-join" class="list-group-item list-group-item-action">{{'League joined'}}</a>
                         </div>
                     </div>
 
