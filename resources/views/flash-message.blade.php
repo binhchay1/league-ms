33396e9024ba77f02449452b9a34640e158044bf
binhchay1 <binhchay1@gmail.com>
2025-03-21 03:29:12 +0000

    change flash

--- resources/views/flash-message.blade.php
+++ resources/views/flash-message.blade.php
@@ -13,7 +13,6 @@
     </div>
 @endif
 
-
 @if ($message = Session::get('warning'))
     <div class="alert alert-warning alert-block">
         <button type="button" class="close" data-dismiss="alert">×</button>
