9ada6d6762ca124123fc61f07b5d073ff2ad6a03
binhchay1 <binhchay1@gmail.com>
2025-03-21 03:57:31 +0000

    test final

--- resources/views/flash-message.blade.php
+++ resources/views/flash-message.blade.php
@@ -4,12 +4,14 @@
         <strong>{{ $message }}</strong>
     </div>
 @endif
+
 @if ($message = Session::get('error'))
     <div class="alert alert-danger alert-block">
         <button type="button" class="close" data-dismiss="alert">×</button>
         <strong>{{ $message }}</strong>
     </div>
 @endif
+
 @if ($message = Session::get('warning'))
     <div class="alert alert-warning alert-block">
         <button type="button" class="close" data-dismiss="alert">×</button>
