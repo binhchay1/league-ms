565fbf57a5f73c252575b701e8a01f13ada06787
binhchay1 <binhchay1@gmail.com>
2025-03-24 04:46:45 +0000

    fix import meta.env

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -257,7 +257,6 @@
                             </ul>
                         </li>
 
-
                         <li class="nav-item">
                             <a href="#" class="nav-link">
                                 <i class="nav-icon fas fa-clipboard"></i>
